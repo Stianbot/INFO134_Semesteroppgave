6bef447a1773ea435d401842b3ec0186b4ff9727
Stianbot <36413091+Stianbot@users.noreply.github.com>
2019-04-10 22:02:11 +0200

    Fjernet globale variabler

--- master.js
+++ master.js
@@ -59,11 +59,10 @@ function getIDs(data){
 }
 
 function getInfo(data, id){
-    let regex = /\d{4}/;
-    let search_key = id.match(regex);
+    let search_key =regex_check(id);
     if (search_key !== null){
         for (let value in data["elementer"]){
-            if (search_key[0] === data["elementer"][value]["kommunenummer"] ){
+            if (search_key === data["elementer"][value]["kommunenummer"] ){
                 data["elementer"][value].name = value;
                 return data["elementer"][value]
             }
@@ -74,15 +73,28 @@ function getInfo(data, id){
     }
 }
 
-function overview(){
+function regex_check(id) {
+    let regex = /\d{4}/;
+    return id.match(regex)[0];
+}
+
+function overview(population){
+    let placement = document.getElementById("overview_list");
+    while (placement.firstChild){
+        placement.removeChild(placement.firstChild)
+    }
+
+    let list = [];
     let id_list = population.getIDs();
     let name_list = population.getNames();
     let total_population = calculate_population(population.data);
     for (let i = 0; i < id_list.length; i++) {
         let li = document.createElement("LI");
         li.appendChild(document.createTextNode(name_list[i]+", "+id_list[i]+", "+total_population[i]));
-        document.getElementById("overview_list").appendChild(li);
+        placement.appendChild(li);
+        list.push([name_list[i],id_list[i],total_population[i]])
     }
+    return list
 }
 
 function calculate_population(data){
@@ -95,15 +107,42 @@ function calculate_population(data){
     return array
 }
 
+function get_some_details(data){
+    let input = regex_check(document.getElementById("input").value);
+    if (input !== null){
+        let result = data.getInfo(input);
+        console.log(result);
 
-let population = new Search(befolkning, "population");
-let employed = new Search(sysselsatte, "employed");
-let education = new Search(utdanning, "education");
+        document.getElementById("details_data").appendChild(document.createTextNode(result.name + " " + result.kommunenummer + " "));
+    }
+    if (input === null){
+        throw "Feil: Skriv inn et gyldig kommunenummer"
+    }
+}
 
-function run() {
+function details(data){
+    get_some_details(data)
+}
+
+
+
+function program() {
+    let population = new Search(befolkning, "population");
+    let employed = new Search(sysselsatte, "employed");
+    let education = new Search(utdanning, "education");
     population.load();
     employed.load();
     education.load();
+
+    document.getElementById("overview_button").addEventListener("click",function () {
+        overview(population)
+    } );
+
+    document.getElementById("details_button").addEventListener("click", function () {
+
+        details(population)
+    })
 }
 
-window.onload = run();
+
+
