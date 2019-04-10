e7ea0c4213aecfc5d5a0c31698e3f4b39a0db6fd
Stianbot <36413091+Stianbot@users.noreply.github.com>
2019-04-10 15:30:43 +0200

    laget load() funksjon

--- master.js
+++ master.js
@@ -20,3 +20,14 @@ function Population(url){
     this.onload = null;
 }
 
+function load(url){
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            let response = JSON.parse(xhr.responseText);
+        }
+    };
+    xhr.send()
+}
+
